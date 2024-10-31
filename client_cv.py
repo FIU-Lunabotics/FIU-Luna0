e73c8d9e77043f1d882b689bdc53b609819e7223
oemolina18 <oemolina18@gmail.com>
2024-10-31 17:42:33 -0400

    commited

--- client_cv.py
+++ client_cv.py
@@ -6,7 +6,35 @@ import pickle
 import struct
 import time
 
-def connect_to_server(host='10.0.0.74', port=8089, max_attempts=5):
+# defines pipeline
+def gstreamer_pipeline(
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink drop=True"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+
+def connect_to_server(host='10.108.116.123', port=8089, max_attempts=5):
     attempt = 0
     while attempt < max_attempts:
         try:
@@ -22,6 +50,7 @@ def connect_to_server(host='10.0.0.74', port=8089, max_attempts=5):
                 print(f"Retrying in 2 seconds...")
                 time.sleep(2)
     raise Exception("Failed to connect to server after multiple attempts")
+
 
 def send_frame(clientsocket, frame):
     try:
@@ -42,7 +71,7 @@ def send_frame(clientsocket, frame):
 
 def main():
     # Initialize video capture
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
     if not cap.isOpened():
         print("Error: Could not open video capture")
         return
