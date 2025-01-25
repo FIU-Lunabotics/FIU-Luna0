433d259dcc2c747aad57ab6e53299c93553ed748
Marcelo Hernandez <marcelohdez.inq@gmail.com>
2025-01-25 14:11:28 -0500

    new: implement args for address and port
    no longer need to edit source to change either, with a default of
    localhost on both server and client

--- controller/server.py
+++ controller/server.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import pickle
 import socket
+import sys
 import time
 
 import evdev
 from evdev import events
 
-HOSTNAME = "localhost"
-PORT = 5000
+from util import DEFAULT_PORT
 
 
 def read_joystick(client_socket: socket.socket):
@@ -53,23 +53,57 @@ def try_handle_client(client_socket: socket.socket):
         time.sleep(2)
 
 
-def start_server(server_socket: socket.socket):
+def start_server(server_socket: socket.socket, ip: str, port: int):
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((HOSTNAME, PORT))
-    server_socket.listen(5)
-    print(f"Server listening on {HOSTNAME}:{PORT}")
-    print("Press Ctrl+C to stop.")
 
+    server_socket.bind((ip, port))
+
+    # arg is how many connections to queue. more than 1 would mean something has gone wrong tbh
+    server_socket.listen(5)
+
+    print(f"Server listening on {ip}:{port}")
+    print("Press Ctrl+C to stop.")
     while True:
         client_socket, (client_ip, client_port) = server_socket.accept()
         print(f"\nNew connection from {client_ip}:{client_port}")
         try_handle_client(client_socket)
 
 
+def print_help():
+    print(f"Usage: {sys.argv[0]} [OPTIONS] [PORT]")
+    print()
+    print("Options:")
+    print("--public     allow external connections")
+    print("--help       prints this info")
+
+
 if __name__ == "__main__":
+    ip = "localhost"
+    port = DEFAULT_PORT
+
+    for arg in sys.argv[1:]:
+        if arg == "--help":
+            print("Lunabotics server script")
+            print()
+            print_help()
+            exit(1)
+        elif arg == "--public":
+            ip = "0.0.0.0"  # allows external connections
+        elif arg.isdigit():
+            if port == DEFAULT_PORT:
+                port = int(arg)
+            else:
+                print(f"Cannot set port twice, remove or fix option {arg}")
+                exit(1)
+        else:
+            print(f'Unknown option: "{arg}"')
+            print()
+            print_help()
+            exit(1)
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        start_server(server_socket)
+        start_server(server_socket, ip, port)
     except KeyboardInterrupt:
         server_socket.shutdown(socket.SHUT_RDWR)
         server_socket.close()
