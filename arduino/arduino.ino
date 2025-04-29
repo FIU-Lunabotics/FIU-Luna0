796ad9ef51e78a36d0353d0dc823cd6ba48ae11b
Frant3K <85142459+Frant3K@users.noreply.github.com>
2025-04-29 16:39:54 -0400

    Revising Data Comm Structure

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -3,15 +3,18 @@
 
 #define PACKET_SIZE 5 // size of packet read from pi in bytes
 
+int droppedBytes = 0;
+int droppedPackets = 0;
+
 CytronMD front_left(PWM_DIR, 3, 2);    //format is (specific h-drive connections), PWM pin, DIR pin
 CytronMD back_left(PWM_DIR, 5, 4);
 CytronMD front_right(PWM_DIR, 6, 7);
 CytronMD back_right(PWM_DIR, 9, 8);
 int count = 0;
 int diff_speed = 0.6;
-int positive_deadzone = 5
-int negative_deadzone = -5
-bool temp_tank_button = 0
+int positive_deadzone = 5;
+int negative_deadzone = -5;
+bool temp_tank_button = 0;
 
 //controller input
 // int left_x = 0;
@@ -32,7 +35,8 @@ bool south_button = 0;
 bool west_button = 0;
 bool left_bumper = 0;
 bool right_bumper = 0;
-int (*tank_drive_button)() = data.get_north_button
+char trigger = 0;
+
 bool tank_drive_mode = false;
 
 //bm = bitmasks
@@ -52,6 +56,7 @@ private:
   byte joy_left_x;
   byte joy_left_y;
   byte joy_right_y;
+  char trigger;
   byte end_byte;
 
 public:
@@ -62,6 +67,7 @@ public:
     this->joy_left_y = 127;
     this->joy_right_x = 127;
     this->joy_right_y = 127;
+    this->trigger = 0;
     this->end_byte_byte = 0b00000000;
   }
 
@@ -71,18 +77,35 @@ public:
     byte barr[PACKET_SIZE];
     int32_t result = Serial.readBytes(barr, PACKET_SIZE);
 
+    if (count == 3) {
+      count = 0;
+    }
+    
     if (result < PACKET_SIZE) {
       Serial.print("WARN: STDIN is either empty or recieved < than expected bytes\n");
       count += 1;
+      droppedBytes += PACKET_SIZE - result;
+      droppedPackets += 1;
       return -1;
+      
     }
-    elif (barr[0]>>6 != count || barr[PACKET_SIZE - 1]&0b00000011 != count) {
-      Serial.print("ERROR: First and last byte Identifier are not "); Serial.print(count); Serial.print(". Skipping\n");
+    else if (barr[0]&start_byte_bm != 0 || barr[PACKET_SIZE - 1]&end_byte_bm != 0) {
+      Serial.print("ERROR: First and last byte spacer isn't 0. Skipping\n");
+      droppedBytes += 6;
+      droppedPackets += 1;
       count += 1;
       return -1;
     }
-    elif (barr[0]&start_byte_bm != 0 || barr[PACKET_SIZE - 1]&end_byte_bm != 0) {
-      Serial.print("ERROR: First and last byte spacer isn't 0. Skipping\n");
+    else if (barr[0]>>6 != count || barr[PACKET_SIZE]&0b00000011 != count) {
+      Serial.print("ERROR: First and last byte Identifier are not "); Serial.print(count); Serial.print(". Skipping\n");
+      if (count > barr[0]>>6) {
+        droppedBytes += 4 - count + barr[0]>>6 + 1;
+        droppedPackets += 1;
+      }
+      else {
+        droppedBytes +=  barr[0]>>6 - count;
+        droppedPackets += 1;
+      }
       count += 1;
       return -1;
     }
@@ -94,14 +117,18 @@ public:
     this->joy_left_x = barr[1];
     this->joy_left_y = barr[2];
     this->joy_right_y = barr[3];
-    this->end_byte = barr[4];
+    this->trigger = barr[4];
+    this->end_byte = barr[PACKET_SIZE];
 
+    /*
     Serial.print("Start: "); Serial.print(this->start_byte);
     Serial.print(" lx: "); Serial.print(this->joy_left_x);
     Serial.print(" ly: "); Serial.print(this->joy_left_y);
     Serial.print(" ry: "); Serial.print(this->joy_right_y);
+    Serial.print("Trigger: "); Serial.print(this->trigger);
     Serial.print("End: "); Serial.print(this->end_byte);
     Serial.print("\n");
+    */
   }
 
   // Access methods
@@ -119,6 +146,8 @@ public:
 
 PiData data; // new instance of PiData class to store our latest data
 
+int (*tank_drive_button)() = data.get_north_button;
+
 void setup() {
   Serial.begin(9600); // Start serial communication
 }
@@ -127,7 +156,7 @@ void loop(){
   data.update();
   if (tank_drive_button == 1 && temp_tank_button == 0){!tank_drive_mode};
   temp_tank_button = tank_drive_button;
-  if (tank_drive_mode == 1){
+  if (tank_drive_mode == 1){`
     tank_drive(data);
   }
   else {
@@ -155,18 +184,19 @@ void tank_drive(const PiData& data) {
 
 void differential_drive(const PiData& data) {
   int left_pwm = map(data.get_joy_left_y(), 0, 255, -255, 255);   // Speed for left motors (same for both)
-  int left_x   = map(data.get_joy_left_x(), 0, 255, -255, 255);
+  int left_joy_x = map(data.get_joy_left_x(), 0, 255, -255, 255);
   
   if (negative_deadzone < left_pwm < positive_deadzone) {
     left_pwm = 0;
   }
 
-  int right_pwm = left_pwm
+  int right_pwm = left_pwm;
   
   // Adjust speeds based on right_x (turning)
-  if (left_x > positive_deadzone) {
+  if (left_joy_x > positive_deadzone) {
     right_speed *= diff_speed*(left_x/255);        // Move right motors slower when joystick is moved right
-  } else if (left_x < negative_deadzone) {
+  } 
+  else if (left_joy_x < negative_deadzone) {
     left_speed *= diff_speed*(left_x/255)*(-1);         // Move left motors slower when joystick is moved left
   }
 
