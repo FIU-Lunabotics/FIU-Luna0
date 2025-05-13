ed4a9a82d20cb193dd4d26d594255c1b989b793f
Carlos Carroz <125514134+CarlosCarroz@users.noreply.github.com>
2025-05-13 15:16:31 -0400

    Update arduino.cpp

--- arduino.cpp
+++ arduino.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <CytronMotorDriver.h>
 
-#define PACKET_SIZE 8 // size of packet read from pi in bytes
+#define PACKET_SIZE 5 // size of packet read from pi in bytes
 
 CytronMD front_left(PWM_DIR, 3, 2);    //format is (specific h-drive connections), PWM pin, DIR pin
 CytronMD back_left(PWM_DIR, 5, 4);
@@ -12,89 +12,141 @@ CytronMD digger(PWM_DIR, 13, 12);
 const int linear_actuator_up = 10;
 const int linear_actuator_down = 11;
 
+//bm = bitmasks
+const int north_button_bm = 0b10000000;
+const int right_bumper_bm = 0b01000000;
+const int left_bumper_bm  = 0b00100000;
+const int start_byte_bm   = 0b00111000;
+const int end_byte_bm     = 0b00011100;
+const int south_button_bm = 0b00000100;
+const int east_button_bm  = 0b00000010;
+const int west_button_bm  = 0b00000001;
+
+bool idle = false;
+bool tank_button_temp = false;
+bool tank_mode = false;
+
+const int negative_deadzone = -5;
+const int positive_deadzone =  5;
+
 class PiData {
 private:
-  bool tank_mode;
+  byte start_byte;
   byte joy_left_x;
   byte joy_left_y;
-  byte joy_right_x;
   byte joy_right_y;
-  byte dX;
-  byte dY; //cCHANGE TO MATCH PYTHON 
+  byte trigger;
+  byte end_byte; //cCHANGE TO MATCH PYTHON 
 
 public:
   /// Constructs a new empty PiData object
   PiData() {
-    this->tank_mode = true; //false;
+    this->start_byte = 0b00000000; //false;
     this->joy_left_x = 127; // assume centered
     this->joy_left_y = 127;
-//  this->joy_right_x = 127;
     this->joy_right_y = 127;
-    this->dX = 127;
-    this->dY = 127;
+    this->trigger = 0;
+    this->end_byte = 0b00000000;
   }
 
   /// Attempts to read new data from the Pi in input buffer
   /// returns number of bytes read if successful, else -1
   int32_t update() {
     byte barr[PACKET_SIZE];
-    int32_t result = Serial.readBytes(barr, PACKET_SIZE);
+    int32_t result = Serial.readBytes(barr, PACKET_SIZE+1);
 
-    if (result < PACKET_SIZE) {
-      Serial.print("WARN: STDIN is either empty or recieved < than expected bytes\n");
+    if (result < PACKET_SIZE+1) {
+      if (idle == false){
+        Serial.print("WARN: STDIN is either empty or recieved < than expected bytes\n");
+        idle = true;
+      }
+      return -1;
+    }
+    idle = false;
+
+    if (barr[0] & start_byte_bm != 0b00101000 || barr[PACKET_SIZE] & end_byte_bm != 0b00010100) {
+      Serial.print("ERROR: First and last bytes spacer are not correct. Skipping\n");
       return -1;
     }
 
-    if (barr[0] != 255 || barr[PACKET_SIZE - 1] != 255) {
-      Serial.print("ERROR: First and last bytes are not 255. Skipping\n");
-      return -1;
-    }
-
-    this->tank_mode = barr[1];
+    this->start_byte = barr[1];
     this->joy_left_x = barr[2];
     this->joy_left_y = barr[3];
-    this->dY = barr[4];
-    this->joy_right_y = barr[5];
-    this->dX = barr[6];
-    //this->dY = barr[7];
+    this->joy_right_y = barr[4];
+    this->trigger = barr[5];
+    this->end_byte = barr[6];
 
-    Serial.print("tank: ");
-    Serial.print(this->tank_mode);
+    //Serial.print("tank: ");
+    //Serial.print(this->tank_mode);
     Serial.print(" lx: ");
     Serial.print(this->joy_left_x);
     Serial.print(" ly: ");
     Serial.print(this->joy_left_y);
-    Serial.print(" rx: ");
-    Serial.print(this->joy_right_x);
+    //Serial.print(" rx: ");
+    //Serial.print(this->joy_right_x);
     Serial.print(" ry: ");
     Serial.print(this->joy_right_y);
-    Serial.print("Dpad x: ");
-    Serial.print(this->dX);
-    Serial.print("Dpad y: ");
-    Serial.print(this->dY);
+    //Serial.print("Dpad x: ");
+    //Serial.print(this->dX);
+    //Serial.print("Dpad y: ");
+    //Serial.print(this->dY);
     Serial.print("\n");
+
+    Serial.end();
+    Serial.begin(9600);
 
     return result;
   }
 
   // Access methods
-  bool get_tank_mode() { return this->tank_mode; }
-  byte get_joy_left_x() { return this->joy_left_x; }
-  byte get_joy_left_y() { return this->joy_left_y; }
-  byte get_joy_right_x() { return this->joy_right_x; }
-  byte get_joy_right_y() { return this->joy_right_y; }
-  byte get_dpad_x() {return this->dX;}
-  byte get_dpad_y() {return this->dY;}
+  bool get_south_button() { return this->start_byte&south_button_bm; }
+  bool get_east_button() { return this->start_byte&east_button_bm; }
+  bool get_west_button() { return this->start_byte&west_button_bm; }
+  bool get_north_button() { return this->end_byte&north_button_bm; }
+  bool get_left_bumper() { return this->end_byte&left_bumper_bm; }
+  bool get_right_bumper() { return this->end_byte&right_bumper_bm; }
+  int get_joy_left_x() { return this->joy_left_x; }
+  int get_joy_left_y() { return this->joy_left_y; }
+  int get_joy_right_y() { return this->joy_right_y; }
+  int get_trigger() { return this->trigger; } 
 
 };
 
 
 PiData data; // new instance of PiData class to store our latest data
 
-bool tank_set = PiData().get_tank_mode(); //allows us to flip the tank_mode
+void setup() {
+  Serial.begin(9600); // Start serial communication
+}
+
+void loop(){
+  data.update();
+  delayMicroseconds(1);
+  get_tank_state();
+  if(tank_mode) {tank_drive(data);}
+  else {differential_steering(data);}
+  digging(data); 
+}                         
+
+void get_tank_state(){
+  bool tank_button = data.get_north_button();
+  if (tank_button == true && tank_button_temp == false){
+    tank_mode = !tank_mode;
+  }
+  tank_button_temp = tank_button;
+
+}
+
 void tank_drive(PiData& data) {
   int left_pwm = map(data.get_joy_left_x(), 0, 255, -255, 255);     // Map joystick values (-min to max) to the PWM range (-255 to 255)
   int right_pwm = map(data.get_joy_left_y(), 0, 255, -255, 255);
+
+  if (negative_deadzone < left_pwm < positive_deadzone){
+    left_pwm = 0;
+  }
+  if (negative_deadzone < right_pwm < positive_deadzone){
+    right_pwm = 0;
+  }
 
   front_left.setSpeed(left_pwm);    // Set motors speeds
   back_left.setSpeed(left_pwm);
@@ -105,18 +157,20 @@ void tank_drive(PiData& data) {
 void differential_steering(PiData& data) {
   int left_x = map(data.get_joy_left_x(), 0, 255, -255, 255);
   int left_y = map(data.get_joy_left_y(), 0, 255, -255, 255);
-// right_x
-  int right_y = map(data.get_joy_right_y(), 0, 255, -255, 255);
+
+  if (negative_deadzone < left_y < positive_deadzone){
+    left_y = 0;
+  }
 
   // Speed variables for both sides
-  int left_speed = map(data.get_joy_left_y(), 0, 255, -255, 255);   // Speed for left motors (same for both)
-  int right_speed = map(data.get_joy_right_y(), 0, 255, -255, 255);
+  int left_speed = left_y;   // Speed for left motors (same for both)
+  int right_speed = left_speed;
 
   // Adjust speeds based on right_x (turning)
   if (left_x > 0) {
-    right_speed *= 0.6;        // Move right motors slower when joystick is moved right
+    right_speed *= map(left_x, -255, 255, 1.6, 0.6 );        // Move right motors slower when joystick is moved right
   } else if (left_x < 0) {
-    left_speed *= 0.6;         // Move left motors slower when joystick is moved left
+    left_speed *= map(left_x, -255, 255, 0.6, 1.6 );         // Move left motors slower when joystick is moved left
   }
 
   front_left.setSpeed(left_speed);
@@ -126,26 +180,21 @@ void differential_steering(PiData& data) {
 }
 
 void digging(PiData& data){
-  int dpad_x = map(data.get_dpad_x(),-1,1,0,255);
-  int dpad_y = map(data.get_dpad_y(),1,-1,0,255);
+  int trigger = data.get_trigger();
+  bool bumper_left = data.get_left_bumper();
+  bool bumper_right = data.get_right_bumper();
 
-  if(dpad_x == 0) {tank_set = !tank_set;}
-  if(dpad_x > 127) {digger.setSpeed(dpad_x);}
-
-  
-  if(dpad_y ==255) {digitalWrite(linear_actuator_up, HIGH); digitalWrite(linear_actuator_down, LOW);}
-  else if(dpad_y == 0) {digitalWrite(linear_actuator_down, HIGH); digitalWrite(linear_actuator_up, LOW);}
-  else {digitalWrite(linear_actuator_down, LOW); digitalWrite(linear_actuator_up, LOW);}
-}
-
-void setup() {
-  Serial.begin(115200); // Start serial communication
-}
-
-void loop(){
-  data.update();
-  delay(1);
-  digging(data); 
-  if(tank_set) {tank_drive(data);}
-  else {differential_steering(data);}
+  if (bumper_left == true) {
+    analogWrite(linear_actuator_down , HIGH);
+  }
+  else {
+    analogWrite(linear_actuator_down , LOW);
+  }
+  if (bumper_right == true) {
+    analogWrite(linear_actuator_up, HIGH);
+  }
+  else{
+    analogWrite(linear_actuator_up , LOW);
+  }
+  digger.setSpeed(trigger);
 }
