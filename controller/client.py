4e739fb541093c1f09e8bda2ef4ad1c882221ce8
Carlos Carroz <125514134+CarlosCarroz@users.noreply.github.com>
2025-05-15 15:59:59 -0400

    Add files via upload

---
+++ controller/client.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+import socket
+import sys
+import time
+import pickle
+
+import evdev
+import evdev.ecodes as ecodes
+
+import consts
+from rover_state import RoverState
+from event import AxisEvent, ButtonEvent
+
+DELAY_US = 50000  # time to delay sending state to server
+
+
+def read_joystick(controller: evdev.InputDevice, c_socket: socket.socket):
+    """Reads events from given device, then sends state to given socket"""
+    axis_info = controller.capabilities(absinfo=True)[ecodes.EV_ABS]
+    state = RoverState()  # track current rover state
+
+    last_usec = 0  # track when last event was
+    # update state with each input event received, and send latest state when enough time
+    # has elapsed, to not overload server/arduino
+    for input in controller.read_loop():
+        # create correspoding event object depending on input type
+        match input.type:
+            case ecodes.EV_ABS:  # absolute (joysticks)
+                event = AxisEvent(input.code, input.value, axis_info)
+            case ecodes.EV_KEY:  # key (button)
+                event = ButtonEvent(input.code, input.value)
+            case _:  # not an event we care for
+                continue
+
+        state.take_event(event)
+
+        # send current state over when enough time has passed
+        if input.usec > last_usec + DELAY_US or input.usec < last_usec:
+            c_socket.sendall(pickle.dumps(state))
+            last_usec = input.usec
+            print(state)
+
+
+def find_controller() -> evdev.InputDevice:
+    """
+    Selects first device with axis and button capabilities as controller and
+    returns that, otherwise raises FileNotFoundError
+    """
+    controller: evdev.InputDevice
+    for path in evdev.list_devices():
+        device = evdev.InputDevice(path)
+        # check if device has axis movement (joysticks)
+        capabilities = device.capabilities(absinfo=False)
+        if ecodes.EV_ABS in capabilities and ecodes.EV_KEY in capabilities:
+            controller = device
+            break
+    else:  # no controller found!
+        raise FileNotFoundError
+
+    return controller
+
+
+def connect_to_server(c_socket: socket.socket, ip: str, port: int):
+    """
+    Only returns when connection is lost. And therefore should be retried.
+    """
+    c_socket.connect((ip, port))
+    print("\nConnected successfully.")
+
+    # read (and send) controller input endlessly
+    first_fail = True
+    while True:  # preferred over recursively calling to avoid stack overflow
+        try:
+            controller = find_controller()
+            print(f"Controller found: {controller.name}")
+
+            read_joystick(controller, c_socket)
+        except FileNotFoundError:
+            if first_fail:
+                print("Controller not found, connect pls.")
+        except BrokenPipeError:
+            print("Failed to send controller event, did client disconnect?")
+            break
+        except OSError as err:
+            if err.errno == 19:
+                print("Oops! controller no longer exists, did it disconnect?")
+            else:
+                raise
+        finally:
+            first_fail = False
+
+        time.sleep(2)
+
+
+def fatal_help(message: str):
+    """
+    Prints help with the given message at the top,
+    then exits with error status (1)
+    """
+    print(message)
+    print()
+    print(f"Usage: {sys.argv[0]} [SERVER_IP]:[PORT] [OPTIONS]")
+    print()
+    print("Options:")
+    print("--help       prints this page")
+    exit(1)
+
+
+if __name__ == "__main__":
+    server_ip = "localhost"
+    server_port = consts.DEFAULT_PORT
+
+    for arg in sys.argv[1:]:
+        if arg == "--help":
+            fatal_help("Lunabotics controller client script.")
+        elif not arg.startswith("--"):
+            split = arg.split(":")
+            if split[0]:  # only set if ip exists, e.g. ignore for input ":2121"
+                server_ip = split[0]
+            if len(split) > 1:
+                if not split[1].isdigit():
+                    fatal_help(f'Port "{split[1]}" is not valid in {arg}')
+                server_port = int(split[1])
+        else:
+            fatal_help(f'Unknown option: "{arg}"')
+
+    print(f"Connecting to {server_ip}:{server_port} (Press Ctrl+C to stop.)")
+    try:
+        while True:
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                connect_to_server(client_socket, server_ip, server_port)
+                print("Oops! Connection lost.")
+            except ConnectionRefusedError:
+                print("Oops, connection was refused, is the server up?")
+            except ConnectionResetError:
+                print("Oops, connection reset, did server restart? Trying again.")
+            except socket.gaierror:
+                fatal_help(f'Invalid server IP: "{server_ip}"')
+                break
+            finally:
+                client_socket.close()
+            time.sleep(3)
+    except KeyboardInterrupt:
+        print("\nFinished gracefully.")
---
+++ controller/event.py
@@ -0,0 +1,115 @@
+"""
+Controller event abstractions
+"""
+
+from evdev import ecodes
+
+# dict keys are gotten from /usr/include/linux/input-event-codes.h
+
+
+class AxisEvent:
+    def __init__(self, code: int, value: int, axis_info) -> None:
+        """
+        Converts the given data into a normalized axis event, giving values only
+        from 0 to 255.
+        """
+        self._code = code
+        self._value = value
+
+        if not (self.dpad_x() or self.dpad_y()):
+            normalized: float
+            for axis, info in axis_info:
+                if axis == code:
+                    normalized = ((value - info.min) / (info.max - info.min)) * 255
+                    break
+            else:
+                raise IndexError(f"No info available for axis {code}?")
+
+            self._value = int(normalized)
+
+    def value(self) -> int:
+        """
+        Returns a value from 0 to 255, 127 being the center.
+        """
+        return self._value
+
+    def dpad_x(self) -> bool:
+        return self._code == ecodes.ecodes["ABS_HAT0X"]
+
+    def dpad_y(self) -> bool:
+        return self._code == ecodes.ecodes["ABS_HAT0Y"]
+
+    def joy_left_x(self) -> bool:
+        return self._code == ecodes.ecodes["ABS_X"]
+
+    def joy_left_y(self) -> bool:
+        return self._code == ecodes.ecodes["ABS_Y"]
+
+    def joy_right_x(self) -> bool:
+        return self._code == ecodes.ecodes["ABS_Z"]
+
+    def joy_right_y(self) -> bool:
+        return self._code == ecodes.ecodes["ABS_RZ"]
+
+    def pressure_ltrigger(self) -> bool:
+        return self._code == ecodes.ecodes["ABS_BRAKE"]
+
+    def pressure_rtrigger(self) -> bool:
+        return self._code == ecodes.ecodes["ABS_GAS"]
+
+
+class ButtonEvent:
+    def __init__(self, code: int, value: int, y_north: bool = True) -> None:
+        """
+        y_north: swaps button self._codes x with y, defaults to True
+        """
+        if y_north:
+            if code == ecodes.ecodes["BTN_X"]:
+                code = ecodes.ecodes["BTN_Y"]
+            elif code == ecodes.ecodes["BTN_Y"]:
+                code = ecodes.ecodes["BTN_X"]
+
+        self._code = code
+        self._value = value
+
+    def value(self) -> int:
+        """
+        Returns 0 if button is released, 1 if pressed.
+        """
+        return self._value
+
+    def button_north(self) -> bool:
+        return self._code == ecodes.ecodes["BTN_NORTH"]
+
+    def button_east(self) -> bool:
+        return self._code == ecodes.ecodes["BTN_EAST"]
+
+    def button_south(self) -> bool:
+        return self._code == ecodes.ecodes["BTN_SOUTH"]
+
+    def button_west(self) -> bool:
+        return self._code == ecodes.ecodes["BTN_WEST"]
+
+    def button_lbumper(self) -> bool:
+        return self._code == ecodes.ecodes["BTN_TL"]
+
+    def button_rbumper(self) -> bool:
+        return self._code == ecodes.ecodes["BTN_TR"]
+
+    def button_ltrigger(self) -> bool:
+        return self._code == ecodes.ecodes["BTN_TL2"]
+
+    def button_rtrigger(self) -> bool:
+        return self._code == ecodes.ecodes["BTN_TR2"]
+
+    def button_select(self) -> bool:
+        return self._code == ecodes.ecodes["BTN_SELECT"]
+
+    def button_start(self) -> bool:
+        return self._code == ecodes.ecodes["BTN_START"]
+
+    def button_lstick(self) -> bool:
+        return self._code == ecodes.ecodes["BTN_THUMBL"]
+
+    def button_rstick(self) -> bool:
+        return self._code == ecodes.ecodes["BTN_THUMBR"]
